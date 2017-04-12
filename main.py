0fb242da65232fb0f801a42ed3dc1cf9292e0bcf
Rob Sawyer <robscottsawyer@gmail.com>
2017-04-12 15:50:33 -0400

    Initialization of the environment in main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mab_environment as mabe
 #Main file for running Agent/Environment interactions
 
 # General Thompson Sampling Algorithm
@@ -7,3 +8,6 @@
 #   environment.get_reward
 #   agent.update
 # results
+
+env = mabe.MultiArmedBanditEnvironment(k_arms=5, hyper_a=0.5, hyper_b=0.5)
+print env.reward_model
